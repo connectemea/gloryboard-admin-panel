bd02282ec05c846c7a44a6bb21587012ddc7bf78
dilshad360 <dilshad360.dev@gmail.com>
2024-12-29 07:50:55 +0530

    feat: image cropper

--- src/components/modals/participantModal.jsx
+++ src/components/modals/participantModal.jsx
@@ -1,44 +1,65 @@
-import React, { useContext } from 'react'
-import { useFormik } from 'formik'
-import { DialogHeader, Dialog, DialogContent, DialogDescription, DialogTitle, DialogTrigger } from '@/components/ui/dialog'
-import { Button } from '@/components/ui/button'
-import { Input } from '@/components/ui/input'
-import { Pencil, Plus } from 'lucide-react'
-import { useModel } from '@/hooks/useModel'
-import { departmentOptions, genderOptions, yearOptions } from '@/constants/options'
-import { participantValidationSchema, validationSchema } from '@/constants/validationSchemas'
-import { initalValue, participantInitalValue } from '@/constants/initalValue'
-import SelectInput from '../common/SelectInput'
-import { useCreateUser, useUpdateUser } from '@/services/mutation/userMutations'
-import { DepartmentOptionsContext } from '@/context/departmentContext'
-import { AuthContext } from '@/context/authContext'
-import extractDepartment from '@/utils/extractDepartment'
+import React, { useContext, useRef, useState } from 'react';
+import { useFormik } from 'formik';
+import { DialogHeader, Dialog, DialogContent, DialogDescription, DialogTitle, DialogTrigger } from '@/components/ui/dialog';
+import { Button } from '@/components/ui/button';
+import { Input } from '@/components/ui/input';
+import { Pencil, Plus } from 'lucide-react';
+import { useModel } from '@/hooks/useModel';
+import { departmentOptions, genderOptions, yearOptions } from '@/constants/options';
+import { participantValidationSchema } from '@/constants/validationSchemas';
+import { participantInitalValue } from '@/constants/initalValue';
+import SelectInput from '../common/SelectInput';
+import { useCreateUser, useUpdateUser } from '@/services/mutation/userMutations';
+import { DepartmentOptionsContext } from '@/context/departmentContext';
+import { AuthContext } from '@/context/authContext';
+import extractDepartment from '@/utils/extractDepartment';
+import ImageCropper from '../ImageCropper';
 
 function ParticipantModal({ editMode = false, initialData = {} }) {
-
     const { mutate: createUser } = useCreateUser();
     const { mutate: updateUser } = useUpdateUser();
-    const { isOpen, openModal, closeModal } = useModel()
+    const { isOpen, openModal, closeModal } = useModel();
 
     const { auth } = useContext(AuthContext);
     const { data } = useContext(DepartmentOptionsContext);
 
-    // Initial form values
-    const formik = useFormik({
-        initialValues: editMode ? { ...participantInitalValue, ...initialData, year_of_study: String(initialData.year_of_study) } : participantInitalValue,
-        validationSchema: participantValidationSchema,
-        validateOnBlur: false,
-        onSubmit: (values) => {
-            console.log(editMode ? 'Updated Data:' : 'New Data:', values)
-            editMode ? updateUser(values) : createUser({ ...values, user_type: 'member' });
-            handleCloseDialog()
-        },
-    })
+    const [image, setImage] = useState(null);
+    const [croppedImage, setCroppedImage] = useState(null);
+    const [isCropDialogOpen, setIsCropDialogOpen] = useState(false);
+
+
+    const handleFileChange = (event) => {
+        const file = event.target.files[0];
+        if (file) {
+            const reader = new FileReader();
+            reader.onload = () => {
+                setImage(reader.result);
+                setIsCropDialogOpen(true); // Open crop dialog after selecting an image
+            };
+            reader.readAsDataURL(file);
+        }
+    };
 
     const handleCloseDialog = () => {
         formik.resetForm();
         closeModal();
-    }
+        setImage(null);
+        setCroppedImage(null);
+    };
+
+
+    const formik = useFormik({
+        initialValues: editMode
+            ? { ...participantInitalValue, ...initialData, year_of_study: String(initialData.year_of_study) }
+            : participantInitalValue,
+        validationSchema: participantValidationSchema,
+        validateOnBlur: false,
+        onSubmit: (values) => {
+            console.log(editMode ? 'Updated Data:' : 'New Data:', values);
+            editMode ? updateUser(values) : createUser({ ...values, user_type: 'member' });
+            handleCloseDialog();
+        },
+    });
 
     return (
         <Dialog open={isOpen} onOpenChange={(open) => (open ? openModal() : handleCloseDialog())}>
@@ -61,7 +82,6 @@ function ParticipantModal({ editMode = false, initialData = {} }) {
                     </DialogDescription>
                 </DialogHeader>
                 <form onSubmit={formik.handleSubmit} className="space-y-2">
-                    {/* Name input field */}
                     <Input
                         name="name"
                         label="Name"
@@ -74,20 +94,22 @@ function ParticipantModal({ editMode = false, initialData = {} }) {
                         <div className="text-red-500 text-sm">{formik.errors.name}</div>
                     )}
 
-                    {/* Department select input */}
                     <SelectInput
                         label="Department"
                         name="department"
                         value={formik.values.department}
                         onChange={formik.handleChange}
                         onBlur={formik.handleBlur}
-                        options={auth.user.user_type === 'admin' ? extractDepartment.getDepartmentListAll(data) :  extractDepartment.getDepartmentListForRep(data, auth)}
+                        options={
+                            auth.user.user_type === 'admin'
+                                ? extractDepartment.getDepartmentListAll(data)
+                                : extractDepartment.getDepartmentListForRep(data, auth)
+                        }
                     />
                     {formik.touched.department && formik.errors.department && (
                         <div className="text-red-500 text-sm">{formik.errors.department}</div>
                     )}
 
-                    {/* Year select input */}
                     <SelectInput
                         label="Year"
                         name="year_of_study"
@@ -96,7 +118,7 @@ function ParticipantModal({ editMode = false, initialData = {} }) {
                         onBlur={formik.handleBlur}
                         options={yearOptions}
                     />
-                    {formik.touched.year_of_study && formik.errors.year_of_study&& (
+                    {formik.touched.year_of_study && formik.errors.year_of_study && (
                         <div className="text-red-500 text-sm">{formik.errors.year_of_study}</div>
                     )}
 
@@ -112,7 +134,6 @@ function ParticipantModal({ editMode = false, initialData = {} }) {
                         <div className="text-red-500 text-sm">{formik.errors.gender}</div>
                     )}
 
-
                     <Input
                         name="number"
                         label="Phone No"
@@ -125,6 +146,14 @@ function ParticipantModal({ editMode = false, initialData = {} }) {
                         <div className="text-red-500 text-sm">{formik.errors.number}</div>
                     )}
 
+                    {/* Image Picker */}
+                    <div>
+
+                        <Input label="Image" type="file" accept="image/*" onChange={handleFileChange} />
+                        {croppedImage && (
+                            <img src={croppedImage} alt="Cropped Preview" className="w-32 h-32 mt-2 aspect-square object-contain rounded" />
+                        )}
+                    </div>
 
                     <div className="!mt-4 flex justify-end">
                         <Button type="submit" className="mr-2" disabled={formik.isSubmitting}>
@@ -136,8 +165,19 @@ function ParticipantModal({ editMode = false, initialData = {} }) {
                     </div>
                 </form>
             </DialogContent>
+
+
+            {/* Crop Dialog */}
+            <Dialog open={isCropDialogOpen} onOpenChange={setIsCropDialogOpen}>
+                <ImageCropper
+                    image={image}
+                    onSave={(croppedImage) => setCroppedImage(croppedImage)}
+                    onClose={() => setIsCropDialogOpen(false)}
+                />
+            </Dialog>
+
         </Dialog>
-    )
+    );
 }
 
-export default ParticipantModal
+export default ParticipantModal;
