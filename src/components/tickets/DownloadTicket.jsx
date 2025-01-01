62219a5c2618f353b412630f4cc5adf705370450
dilshad360 <dilshad360.dev@gmail.com>
2025-01-01 20:32:24 +0530

    Dialog added for ticket download

--- src/components/tickets/DownloadTicket.jsx
+++ src/components/tickets/DownloadTicket.jsx
@@ -1,11 +1,20 @@
 import React, { useState } from "react";
 import { Button } from "../ui/button";
 import axiosInstance from "@/api/axiosInstance";
-import { Download, Loader2 } from "lucide-react";
+import { Download, FileText, Loader2 } from "lucide-react";
 import { toast } from "sonner";
+import {
+  Dialog,
+  DialogContent,
+  DialogDescription,
+  DialogHeader,
+  DialogTitle,
+  DialogTrigger,
+} from "@/components/ui/dialog";
 
 function DownloadTicket({ id, name }) {
   const [loading, setLoading] = useState(false);
+  const [isDialogOpen, setIsDialogOpen] = useState(false);
 
   const handleDownloadTicket = async () => {
     setLoading(true); // Start the loader
@@ -46,15 +55,33 @@ function DownloadTicket({ id, name }) {
   };
 
   return (
-    <Button
-      variant="outline"
-      size="icon"
-      className="w-8 h-8"
-      disabled={loading}
-      onClick={handleDownloadTicket}
+    <Dialog
+      open={isDialogOpen}
+      onOpenChange={(open) => !loading && setIsDialogOpen(open)}
     >
-      {loading ? <Loader2 className="animate-spin" /> : <Download />}
-    </Button>
+      <DialogTrigger onClick={() => setIsDialogOpen(true)}>
+        <Button variant="outline" size="icon" className="w-8 h-8">
+          <Download />
+        </Button>
+      </DialogTrigger>
+      <DialogContent>
+        <DialogHeader>
+          <DialogTitle>Download Ticket</DialogTitle>
+          <DialogDescription className="py-6  text-center flex justify-center items-center flex-col gap-5">
+          <FileText className="w-32 h-32 opacity-40" />
+          <span>
+          You're about to download the ticket for <b>{name}</b> . Ensure your internet connection is stable to complete the download successfully.
+          </span>
+          </DialogDescription>
+          <Button
+            disabled={loading}
+            onClick={handleDownloadTicket}
+          >
+            {loading ? <Loader2 className="animate-spin" /> : "Download"}
+          </Button>
+        </DialogHeader>
+      </DialogContent>
+    </Dialog>
   );
 }
 
