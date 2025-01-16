c7ba58765e6c0b209cdad8cbbbf0fffce8371d7f
dilshad360 <dilshad360.dev@gmail.com>
2025-01-16 15:13:21 +0530

    hot fix 2

--- src/components/modals/view/eventViewModal.jsx
+++ src/components/modals/view/eventViewModal.jsx
@@ -33,25 +33,25 @@ function EventViewModal({ data = {} }) {
       </DialogTrigger>
       <DialogContent>
         <DialogHeader>
-          <DialogTitle className="truncate max-w-[300px]">{data.name}</DialogTitle>
+          <DialogTitle className="truncate max-w-[300px]">{data?.name}</DialogTitle>
         </DialogHeader>
 
         <div className="border p-4 space-y-1 rounded-md">
           <div className="flex items-center">
             <span className="font-semibold text-white/70 pr-2">Type: </span>
-            <p className="text-white/70 text-sm">{data.event_type.name}</p>
+            <p className="text-white/70 text-sm">{data?.event_type?.name}</p>
           </div>
           <div className="flex items-center">
             <span className="font-semibold text-white/70 pr-2">Event Category: </span>
-            <p className="text-white/70 text-sm">{data.event_category}</p>
+            <p className="text-white/70 text-sm">{data?.event_category}</p>
           </div>
           <div className="flex items-center">
             <span className="font-semibold text-white/70 pr-2">Result Category: </span>
-            <p className="text-white/70 text-sm">{data.result_category}</p>
+            <p className="text-white/70 text-sm">{data?.result_category}</p>
           </div>
           <div className="flex items-center">
             <span className="font-semibold text-white/70 pr-2">Participants Limit: </span>
-            <p className="text-white/70 text-sm">{`${data.min_participants} - ${data.max_participants}`}</p>
+            <p className="text-white/70 text-sm">{`${data?.min_participants} - ${data.max_participants}`}</p>
           </div>
           {data?.start_time &&
             <div className="flex items-center">
