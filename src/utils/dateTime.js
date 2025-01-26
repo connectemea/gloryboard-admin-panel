259fe552e3c50245ae65a21b5e085060583b4bc0
dilshad360 <dilshad360.dev@gmail.com>
2025-01-27 00:28:01 +0530

    fix: handle invalid time in schedule

--- src/utils/dateTime.js
+++ src/utils/dateTime.js
@@ -1,8 +1,19 @@
 export const convertToDateTime = (isoString) => {
+  try {
     const date = new Date(isoString);
+
+    // Check if the date is valid
+    if (isNaN(date.getTime())) {
+      throw new Error("Invalid date");
+    }
+
     return new Intl.DateTimeFormat('en-US', {
-    dateStyle: 'short', 
-      timeStyle: 'short',  // "4:30 AM"
-      timeZone: 'UTC',     // Optional: Use the correct time zone
+      dateStyle: 'short',
+      timeStyle: 'short', // "4:30 AM"
+      timeZone: 'UTC',    // Optional: Use the correct time zone
     }).format(date);
+  } catch (error) {
+    console.error("Invalid date input:", error.message);
+    return "-";
+  }
 };
